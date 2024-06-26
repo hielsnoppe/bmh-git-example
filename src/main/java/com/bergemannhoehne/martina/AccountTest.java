97683fb301faa73facb53672a6bee4aef86d156b
MaPankow <162994783+MaPankow@users.noreply.github.com>
2024-06-26 11:02:06 +0200

    Account Interface

---
+++ src/main/java/com/bergemannhoehne/martina/AccountTest.java
@@ -0,0 +1,11 @@
+package main.java.com.bergemannhoehne.martina;
+
+public class AccountTest implements Account.deposit {
+    
+    @Override
+    public void deposit() {
+        float balance = 0;
+
+
+    }
+}
